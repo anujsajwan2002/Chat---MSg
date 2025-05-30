58e033ade7d49633f1574ae2b613d0613e4687e7
Anuj <sajwananuj2@gmail.com>
2025-05-30 16:30:27 +0530

    chat messaging
    "initial commit"

---
+++ app/src/main/java/com/chat/app/Model/ChatMessage.java
@@ -0,0 +1,37 @@
+package com.chat.app.Model;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class ChatMessage {
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    private Long id;
+    private String sender;
+    private String content;
+}
